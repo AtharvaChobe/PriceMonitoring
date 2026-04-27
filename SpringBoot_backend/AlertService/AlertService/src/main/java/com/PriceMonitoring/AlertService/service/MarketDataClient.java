2583aac6ace335d29f2363dd11664b08ba661c9a
AtharvaChobe <103590543+AtharvaChobe@users.noreply.github.com>
2026-04-27 20:12:15 +0530

    made changes in feign client url

--- SpringBoot_backend/AlertService/AlertService/src/main/java/com/PriceMonitoring/AlertService/service/MarketDataClient.java
+++ SpringBoot_backend/AlertService/AlertService/src/main/java/com/PriceMonitoring/AlertService/service/MarketDataClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "DATA-SERVICE")
+@FeignClient(name = "data-service", url = "${DATA_SERVICE_URL}")
 public interface MarketDataClient {
     @GetMapping("/data")
     double getMarketData(@RequestParam String metal, @RequestParam String city);
